3a85c03a42da6396469485ecbfe2c44787cbc685
jp-quintana <juanquintana1996@gmail.com>
2026-01-15 04:45:34 +0000

    feat: add ioredis

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { REDIS_CLIENT } from './redis/redis.module';
+import Redis from 'ioredis';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject(REDIS_CLIENT) private readonly redis: Redis,
+  ) {}
 
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const response = await this.redis.ping();
+    if (response === 'PONG') console.log('Redis works');
     return this.appService.getHello();
   }
 }

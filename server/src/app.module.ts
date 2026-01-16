2e305594092a6758b1f7a4e2364cd22fdaa0b10a
jp-quintana <juanquintana1996@gmail.com>
2026-01-16 02:42:57 +0000

    feat: add middleware

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
+import { RateLimiterMiddleware } from './middlewares';
 
 @Module({
   imports: [RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RateLimiterMiddleware).forRoutes('*');
+  }
+}

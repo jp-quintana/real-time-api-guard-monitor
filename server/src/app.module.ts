4ad5d14ed51aaefe474640840efec80adb494420
jp-quintana <juanquintana1996@gmail.com>
2026-02-03 03:35:12 +0000

    feat: add users resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,7 @@ import { RateLimiterMiddleware, RecentActivityMiddleware } from './middlewares';
 import { LogsModule } from './modules/logs/logs.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { ConfigModule } from '@nestjs/config';
     RedisModule,
     LogsModule,
     DatabaseModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],

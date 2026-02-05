2e40725113d33ea98786e5c85d6fe28520b5ae38
jp-quintana <juanquintana1996@gmail.com>
2026-02-05 04:37:38 +0000

    feat: add users schema

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/sign-in')
+  async signIn() {
+    return this.authService.signIn();
+  }
+
+  @Post('/sign-up')
+  async signUp() {
+    return this.authService.signUp();
+  }
 }
---
+++ server/src/modules/users/schemas/users.schema.ts
@@ -0,0 +1,9 @@
+import { text } from 'drizzle-orm/pg-core';
+import { uuid } from 'drizzle-orm/pg-core';
+import { pgTable } from 'drizzle-orm/pg-core';
+
+export const users = pgTable('users', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  email: text('email').unique(),
+  password: text('password'),
+});
